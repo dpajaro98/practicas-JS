80dd7de211b1dcce6683ef96a9dbee60573f035d
dpajaro98 <drpajaro7@misena.edu.co>
2022-08-03 18:56:33 -0500

    Practica de Bifurcaciones if y else

---
+++ practica-bifurcaciones-js-1.js
@@ -0,0 +1,8 @@
+let recarga=10000
+let compraPaquete=3000
+var nuevosaldo=recarga-compraPaquete
+if(recarga<compraPaquete){
+    console.log("saldo insuficiente")
+}else{
+    console.log("Recarga Exitosa,saldo restante:"+nuevosaldo+" COP")
+}
